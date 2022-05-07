6d608fc47a04f1768bd75f3635230c8b43d7b1b8
Max Rodriguez <developmax@gmx.ru>
2022-05-06 21:13:29 -0400

    Create Readme.cmd

---
+++ Readme.cmd
@@ -0,0 +1,6 @@
+                                   
+ pip install -r requirements.txt 👈
+ python app.py
+
+[![1.png](https://i.postimg.cc/fyjN0Qq6/1.png)](https://postimg.cc/Cn1tWXgN)
+[![2.png](https://i.postimg.cc/fTTFvdV3/2.png)](https://postimg.cc/DmHBv8dF)
